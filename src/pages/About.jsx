9eded95b7d29d792acdc19e15745a46ad1026958
Lena0912 <elenabelichenko9@gmail.com>
2023-11-11 02:25:11 +0200

    hw

---
+++ src/pages/About.jsx
@@ -0,0 +1,16 @@
+export const About = () => {
+  return (
+    <main>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae voluptate
+        maxime natus commodi asperiores qui sit optio voluptas laboriosam, eius
+        libero nemo sequi delectus. Praesentium voluptas debitis inventore
+        dolorum eaque?Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Tenetur maiores eius impedit temporibus, accusantium labore suscipit,
+        necessitatibus voluptates porro eos, recusandae odit quis in molestias
+        ullam asperiores. Impedit, consequuntur molestias.
+      </p>
+    </main>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <main>
+      <h1>Welcome</h1>
+      <img src="https://via.placeholder.com/960x240" alt="" />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa,
+        voluptatum asperiores exercitationem quam ullam quidem facilis, nostrum
+        fugiat tempore ipsum officiis, ab laudantium. Sit repellat ipsum
+        possimus in fuga blanditiis!
+      </p>
+    </main>
+  );
+};
